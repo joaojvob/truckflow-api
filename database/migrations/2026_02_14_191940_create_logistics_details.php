7d8bbc48327c27ecafbf03bee62db3457c83aa4b
Joao Julio <joaojuliovob@gmail.com>
2026-02-14 16:24:55 -0300

    feat: configuração inicial do projeto com laravel sail e suporte ao postgis

---
+++ database/migrations/2026_02_14_191940_create_logistics_details.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('job_id')->constrained();
+        $table->json('items'); // Ex: {"pneus": true, "oleo": true, "luzes": false}
+        $table->timestamps();
+    });
+
+    Schema::create('incidents', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('job_id')->constrained();
+        $table->enum('type', ['breakdown', 'accident', 'robbery', 'sos']); // O botão de SOS
+        $table->text('description')->nullable();
+        $table->geography('location', 'point', 4326); // Onde aconteceu
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incidents');
+        Schema::dropIfExists('checklists');
+    }
+};
