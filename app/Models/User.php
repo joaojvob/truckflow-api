726ef54854e50b810353820e690f8fe4250a08bd
Joao Julio <joaojuliovob@gmail.com>
2026-02-14 18:02:29 -0300

    feat(auth): adicionar HasApiTokens ao model User
    Integração com Laravel Sanctum para autenticação via token
    na API REST, necessário para o App Flutter.

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, BelongsToTenant; 
+    use HasFactory, Notifiable, BelongsToTenant, HasApiTokens; 
 
     /**
      * The attributes that are mass assignable.
